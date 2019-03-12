44e0cc538b04911fb80efd45b80d5e8eae8ed211
rharshit <rharshit98@gmail.com>
2019-03-13 00:26:45 +0530

    client: Add options to access the API

--- api.h
+++ api.h
@@ -1,2 +1,3 @@
 void test();
+void print_list();
 struct stat get_file_info(char * path);
--- client.cpp
+++ client.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 #include <sys/stat.h>
+#include <sys/types.h>
 
 #include "api.h"
 using namespace std;
@@ -7,15 +8,40 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
 	char path[50];
-	while(1){
-		cout<<"Filename: ";
-	cin>>path;
-	cin.ignore(80,'\n');
-	cout<<get_file_info(path).st_size<<endl;
-	}
-	cout<<"Filename: ";
-	cin>>path;
-	cin.ignore(80,'\n');
-	cout<<get_file_info(path).st_size<<endl;
+	int opt=0;
+	do{
+		cout<<endl<<"1. Get file info"<<endl<<
+			"2. Get files list"<<endl<<
+			"0. Exit"<<endl;
+		cout<<"Option: ";
+		cin>>opt;
+		switch(opt){
+			case 1: 
+			{
+				cout<<"Filename: ";
+				cin>>path;
+				cin.ignore(80,'\n');
+				struct stat attr = get_file_info(path);
+				cout<<endl<<"Size: "<<attr.st_size<<endl;
+				break;
+			}
+			case 2:
+			{
+				/*int sort;
+				cout<<"1. Sort by name"<<endl<<
+					"2. Sort by name"<<endl<<
+					"3. Sort by name"<<endl;
+				cout<<"Option: ";
+				cin>>sort;*/
+				print_list();
+				break;
+			}
+			case 0:
+			{
+				break;
+			}
+		}
+	} while (opt);
+
 	return 0;
 }
